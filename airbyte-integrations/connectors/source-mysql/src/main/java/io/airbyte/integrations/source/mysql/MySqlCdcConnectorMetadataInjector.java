394a1616ff217af8b67eb97c8a779e883ca5ad2f
nguyenaiden <duy@airbyte.io>
2023-07-26 13:58:34 -0700

    onstruct cdc default cursor for mysql

--- airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlCdcConnectorMetadataInjector.java
+++ airbyte-integrations/connectors/source-mysql/src/main/java/io/airbyte/integrations/source/mysql/MySqlCdcConnectorMetadataInjector.java
@@ -4,19 +4,30 @@
 
 package io.airbyte.integrations.source.mysql;
 
+import static io.airbyte.integrations.source.mysql.MySqlSource.CDC_DEFAULT_CURSOR;
 import static io.airbyte.integrations.source.mysql.MySqlSource.CDC_LOG_FILE;
 import static io.airbyte.integrations.source.mysql.MySqlSource.CDC_LOG_POS;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.airbyte.integrations.debezium.CdcMetadataInjector;
+import java.time.Instant;
 
 public class MySqlCdcConnectorMetadataInjector implements CdcMetadataInjector {
 
+  final Instant emittedAt;
+
+  public MySqlCdcConnectorMetadataInjector(final Instant emittedAt) {
+    this.emittedAt = emittedAt;
+  }
+
   @Override
   public void addMetaData(final ObjectNode event, final JsonNode source) {
+    final String cdcDefaultCursor =
+        String.format("%s_%s_%s", emittedAt.toString(), source.get("file").asText(), source.get("pos").asText());
     event.put(CDC_LOG_FILE, source.get("file").asText());
     event.put(CDC_LOG_POS, source.get("pos").asLong());
+    event.put(CDC_DEFAULT_CURSOR, cdcDefaultCursor);
   }
 
   @Override
